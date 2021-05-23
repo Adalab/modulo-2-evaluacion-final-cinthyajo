b989e669a485a22068e822a0007fa10284b36cb4
cinthyajo <cinthya.jo@gmail.com>
2021-05-23 23:08:31 +0200

    Add js functions favourite list and styles

--- javascript/main.js
+++ javascript/main.js
@@ -1,25 +1,25 @@
 "use strict";
-//variable de string
-const imagePlaceholder =
-  "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
+
 const inputTitle = document.querySelector(".js-inputtitle");
 const button = document.querySelector(".js-btn");
 const listTvShow = document.querySelector(".js-list");
 const favouriteList = document.querySelector(".js-favourite");
+const imagePlaceholder =
+  "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
+let favourites = [];
 
 function handlerButtonClick(event) {
-  //Para construir la URL de búsqueda hay que recoger el texto que ha introducido la usuaria en el campo de búsqueda
+  listTvShow.innerHTML = null;
+
   const searchValue = inputTitle.value;
-  //esta es mi url de búsqueda con el valor que busco searchValue. Construir la URL búsqueda...
+
   const url = `http://api.tvmaze.com/search/shows?q=${searchValue}`;
   console.log(url);
-  //aquí ya le paso toda la url creada
+
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      //recorrido toda la lista de resultados de la búsqueda. Fetch me devuelve el resultado de la búsqueda.
       for (let index = 0; index < data.length; index++) {
-        /*console.log(data[index]);*/
         let titleShow = data[index].show.name;
         let imageShow = null;
         if (data[index].show.image != null) {
@@ -27,27 +27,54 @@ function handlerButtonClick(event) {
         } else {
           imageShow = imagePlaceholder;
         }
-        /*console.log(titleShow + " " + imageShow);*/
-
-        listTvShow.innerHTML += `<li onclick="handlerCardClick(this)">
-            <div > 
-            <img src="${imageShow}" alt="" id="js-img" />
-            <h3 id="js-showTitle">${titleShow}</h3>
-            </div>
-        </li>`;
+        listTvShow.appendChild(
+          newHtmlListNodeWithOnClick(titleShow, imageShow)
+        );
       }
-      /*const card = document.querySelector(".js-cardShow");
-      card.addEventListener("click", handlerCardClick);*/
     });
 }
-//element.show.name / element.show.image.medium
-//estoy recibiendo todo el list item. element es el li
-function handlerCardClick(element) {
-  /*con el children 0 porque pueden haber más cosas dentro del div
-  console.log(element.children[0]);*/
-  const newItem = document.createElement("li");
-  newItem.appendChild(element.children[0]);
-  favouriteList.appendChild(newItem);
+
+function handlerCardClick(title, img) {
+  const showSelected = { title: title, img: img };
+  favourites.push(showSelected);
+  setInLocalStorage();
+  buildFavouritesLilst();
+}
+
+function buildFavouritesLilst() {
+  favouriteList.innerHTML = null;
+  for (const favourite of favourites) {
+    favouriteList.appendChild(newHtmlListNode(favourite.title, favourite.img));
+  }
+}
+
+function newHtmlListNode(titleShow, imageShow) {
+  const divItem = document.createElement("div");
+
+  const imageItem = document.createElement("img");
+  imageItem.src = imageShow;
+  imageItem.id = "js-img";
+
+  const h3Item = document.createElement("h3");
+  const h3Content = document.createTextNode(titleShow);
+  h3Item.appendChild(h3Content);
+  h3Item.id = "js-showTitle";
+
+  divItem.appendChild(imageItem);
+  divItem.appendChild(h3Item);
+
+  const liItem = document.createElement("li");
+  liItem.appendChild(divItem);
+
+  return liItem;
+}
+
+function newHtmlListNodeWithOnClick(titleShow, imageShow) {
+  let liItem = newHtmlListNode(titleShow, imageShow);
+  liItem.onclick = function () {
+    handlerCardClick(titleShow, imageShow);
+  };
+  return liItem;
 }
 
 button.addEventListener("click", handlerButtonClick);
