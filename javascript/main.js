159ab97641603fc0daf81c7da6614d1ae7046f4a
cinthyajo <cinthya.jo@gmail.com>
2021-05-25 13:15:58 +0200

    Add final version

--- javascript/main.js
+++ javascript/main.js
@@ -4,17 +4,16 @@ const inputTitle = document.querySelector(".js-inputtitle");
 const button = document.querySelector(".js-btn");
 const listTvShow = document.querySelector(".js-list");
 const favouriteList = document.querySelector(".js-favourite");
-const inputRadioBtn = document.querySelector(".js-radiobtn");
+const eraseBtn = document.querySelector(".js-eraseallbtn");
 const imagePlaceholder =
   "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
 let favourites = [];
 let series = [];
 
-//lista búsqueda derecha
 function handlerButtonClick(event) {
   listTvShow.innerHTML = null;
   const searchValue = inputTitle.value;
-  const url = `http://api.tvmaze.com/search/shows?q=${searchValue}`;
+  const url = `//api.tvmaze.com/search/shows?q=${searchValue}`;
   console.log(url);
 
   fetch(url)
@@ -36,21 +35,15 @@ function paintSeries() {
     } else {
       imageShow = imagePlaceholder;
     }
-    //lista de resultados derecha Problemas para click en evento entonces onclick en elemento
+
     listTvShow.appendChild(
       newHtmlListNodeWithOnClick(titleShow, imageShow, idShow, false)
     );
   }
 }
 
-//función para mi local storage - tienda de camisetas
 function handlerCardClick(title, img, id) {
   console.log(id);
-
-  // busco el id en el array de favoritos >>> find me devuelve elem si no undefined
-  // - Si está: lo saco
-  // - Si no está: lo coloco.
-
   const foundIndex = favourites.findIndex((favourite) => {
     if (favourite.id === id) {
       return true;
@@ -60,39 +53,25 @@ function handlerCardClick(title, img, id) {
   });
 
   if (foundIndex === -1) {
-    // no está en favoritos, lo meto
     const showSelected = { title: title, img: img, id: id };
-    //findIndex o find luego decidir si hago push o no
     favourites.push(showSelected);
   } else {
-    // sí está en favoritos
-    console.log("ya está en fav", foundIndex);
     favourites.splice(foundIndex, 1);
   }
-
-  //findIndex o find luego decidir si hago push o no
-
   paintSeries();
-  //local storage- ejecuto la función
   setInLocalStorage();
   buildFavouritesList();
 }
 
-//lista favoritos izquierda
 function buildFavouritesList() {
   favouriteList.innerHTML = null;
-  //recorro mi array con mis favoritos
   for (const favourite of favourites) {
     favouriteList.appendChild(
       newHtmlListNode(favourite.title, favourite.img, favourite.id, true)
     );
-
-    //de mi array de favoritos elimina todos los elem. comenzando en index 0
-    //let removed = favourites.splice(0);
   }
 }
 
-//Dom avanzado - proceso de crear el <li>
 function newHtmlListNode(titleShow, imageShow, idShow, paintRemoveButton) {
   console.log(titleShow, imageShow, idShow, paintRemoveButton);
   const divItem = document.createElement("div");
@@ -149,34 +128,27 @@ function newHtmlListNodeWithOnClick(
   return liItem;
 }
 
-//función para radio button. getElementsByName: regresa todos los rb del grupo?.
-/*function getCheckedRadio() {
-    let radioButtons = document.getElementsByName("???");
-    const radioBtnValue = inputRadioBtn.value;
-      for (let i = 0; i < radioButtons.length; i++) {
-        if (radioButtons[i].checked) {
-          alert("You checked" + radioButtons[i].//name or id);
-        }
-      }
-    }*/
+function handlerEraseBtnClick(event) {
+  let removed = favourites.splice(0);
+  buildFavouritesList();
+  paintSeries();
+  setInLocalStorage();
+}
 
-//crear const local storage
 const setInLocalStorage = () => {
   const stringifyfavourites = JSON.stringify(favourites);
-  //setItem guardar datos de mi lista favoritos - tienda de camisetas
   localStorage.setItem("favourite", stringifyfavourites);
 };
 
 const getFromLocalStorage = () => {
-  //getItem recuperar datos - tienda de camisetas
   const localStoragefavourites = localStorage.getItem("favourite");
-  //diferente !==
   if (localStoragefavourites !== null) {
     favourites = JSON.parse(localStoragefavourites);
   }
 };
 
 button.addEventListener("click", handlerButtonClick);
+eraseBtn.addEventListener("click", handlerEraseBtnClick);
 
 getFromLocalStorage();
 buildFavouritesList();
