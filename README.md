a25c2fa1feca378b9348e9c04909b8d06f3da5ca
cinthyajo <cinthya.jo@gmail.com>
2021-05-25 11:43:40 +0200

    Add create button, erase favourites, add id parameters

---
+++ README.md
@@ -0,0 +1,21 @@
+Script section:
+
+<!DOCTYPE html>
+<h1 class="js-title title">Buscador de series</h1>
+    <form class="form">
+      <input
+      />
+      <button >Search</button>
+    </form>
+    <h2 >Mis series favoritas</h2>
+    <section >
+      <div >
+        <ul ></ul>
+      </div>
+      <div >
+        <ul ></ul>
+      </div>
+    </section>
+
+JavaScript object and JASON resource
+API
--- javascript/main.js
+++ javascript/main.js
@@ -4,6 +4,7 @@ const inputTitle = document.querySelector(".js-inputtitle");
 const button = document.querySelector(".js-btn");
 const listTvShow = document.querySelector(".js-list");
 const favouriteList = document.querySelector(".js-favourite");
+const inputRadioBtn = document.querySelector(".js-radiobtn");
 const imagePlaceholder =
   "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
 let favourites = [];
@@ -12,9 +13,7 @@ let series = [];
 //lista búsqueda derecha
 function handlerButtonClick(event) {
   listTvShow.innerHTML = null;
-
   const searchValue = inputTitle.value;
-
   const url = `http://api.tvmaze.com/search/shows?q=${searchValue}`;
   console.log(url);
 
@@ -37,9 +36,9 @@ function paintSeries() {
     } else {
       imageShow = imagePlaceholder;
     }
-    //lista de resultados derecha Problemas para click en evento entonces click en elemento
+    //lista de resultados derecha Problemas para click en evento entonces onclick en elemento
     listTvShow.appendChild(
-      newHtmlListNodeWithOnClick(titleShow, imageShow, idShow)
+      newHtmlListNodeWithOnClick(titleShow, imageShow, idShow, false)
     );
   }
 }
@@ -52,17 +51,27 @@ function handlerCardClick(title, img, id) {
   // - Si está: lo saco
   // - Si no está: lo coloco.
 
-  const foundId = favourites.find((favourite) => {
-    if (favourite.id !== undefined) {
+  const foundIndex = favourites.findIndex((favourite) => {
+    if (favourite.id === id) {
       return true;
     } else {
       return false;
     }
   });
 
-  const showSelected = { title: title, img: img, id: id };
+  if (foundIndex === -1) {
+    // no está en favoritos, lo meto
+    const showSelected = { title: title, img: img, id: id };
+    //findIndex o find luego decidir si hago push o no
+    favourites.push(showSelected);
+  } else {
+    // sí está en favoritos
+    console.log("ya está en fav", foundIndex);
+    favourites.splice(foundIndex, 1);
+  }
+
   //findIndex o find luego decidir si hago push o no
-  favourites.push(showSelected);
+
   paintSeries();
   //local storage- ejecuto la función
   setInLocalStorage();
@@ -74,12 +83,18 @@ function buildFavouritesList() {
   favouriteList.innerHTML = null;
   //recorro mi array con mis favoritos
   for (const favourite of favourites) {
-    favouriteList.appendChild(newHtmlListNode(favourite.title, favourite.img));
+    favouriteList.appendChild(
+      newHtmlListNode(favourite.title, favourite.img, favourite.id, true)
+    );
+
+    //de mi array de favoritos elimina todos los elem. comenzando en index 0
+    //let removed = favourites.splice(0);
   }
 }
 
 //Dom avanzado - proceso de crear el <li>
-function newHtmlListNode(titleShow, imageShow, idShow) {
+function newHtmlListNode(titleShow, imageShow, idShow, paintRemoveButton) {
+  console.log(titleShow, imageShow, idShow, paintRemoveButton);
   const divItem = document.createElement("div");
 
   const foundFav = favourites.find((favourite) => {
@@ -106,19 +121,44 @@ function newHtmlListNode(titleShow, imageShow, idShow) {
   divItem.appendChild(imageItem);
   divItem.appendChild(h3Item);
 
+  if (paintRemoveButton === true) {
+    const buttonItem = document.createElement("button");
+    const buttonContent = document.createTextNode("Borrar este favorito");
+    buttonItem.appendChild(buttonContent);
+    buttonItem.onclick = function () {
+      handlerCardClick(titleShow, imageShow, idShow);
+    };
+    divItem.appendChild(buttonItem);
+  }
+
   const liItem = document.createElement("li");
   liItem.appendChild(divItem);
-
   return liItem;
 }
 
-function newHtmlListNodeWithOnClick(titleShow, imageShow, idShow) {
-  let liItem = newHtmlListNode(titleShow, imageShow, idShow);
+function newHtmlListNodeWithOnClick(
+  titleShow,
+  imageShow,
+  idShow,
+  paintRemoveButton
+) {
+  let liItem = newHtmlListNode(titleShow, imageShow, idShow, paintRemoveButton);
   liItem.onclick = function () {
     handlerCardClick(titleShow, imageShow, idShow);
   };
   return liItem;
 }
+
+//función para radio button. getElementsByName: regresa todos los rb del grupo?.
+/*function getCheckedRadio() {
+    let radioButtons = document.getElementsByName("???");
+    const radioBtnValue = inputRadioBtn.value;
+      for (let i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked) {
+          alert("You checked" + radioButtons[i].//name or id);
+        }
+      }
+    }*/
 
 //crear const local storage
 const setInLocalStorage = () => {
